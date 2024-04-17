388a1471741f7f83187eb5805380a0be1b028c07
sleep <49450642+BIoat@users.noreply.github.com>
2024-04-17 16:28:26 +0200

    7.7.4

--- wadbotgui/build.sh
+++
@@ -1,9 +0,0 @@
-cargo build --release --target x86_64-pc-windows-gnu
-cargo build --release 
-cp "./target/x86_64-pc-windows-gnu/release/wadbotgui.exe" ./out/wadbotgui.exe
-cp "./target/release/wadbotgui" ./out/wadbotgui
-cd out 
-strip wadbotgui
-strip wadbotgui.exe
-upx --lzma wadbotgui wadbotgui.exe
-cp -f wadbotgui.exe /home/anon/wadbot/wadbotgui.exe
